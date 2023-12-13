dde080460477c4d60a132f8a7b31aff8ffaa18bd
liyinrong <li.yinrong.acc@outlook.com>
2023-12-13 21:19:57 +0100

    AccGyr sensor now works.

--- X-CUBE-AI/App/tinyfallnet_qat.c
+++ X-CUBE-AI/App/tinyfallnet_qat.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    tinyfallnet_qat.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    Wed Dec 13 09:14:26 2023
+  * @date    Wed Dec 13 20:48:07 2023
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
@@ -42,7 +42,7 @@
 #endif
 
 #undef AI_TOOLS_DATE_TIME
-#define AI_TOOLS_DATE_TIME   "Wed Dec 13 09:14:26 2023"
+#define AI_TOOLS_DATE_TIME   "Wed Dec 13 20:48:07 2023"
 
 #undef AI_TOOLS_COMPILE_TIME
 #define AI_TOOLS_COMPILE_TIME    __DATE__ " " __TIME__
